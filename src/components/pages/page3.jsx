08b87eb76d3fd65e472be302a336aa72caaab6eb
jpguelerazevedo <jpguelerazevedo@gmail.com>
2026-02-26 09:44:34 -0300

    feat: Improve layout and styling of post cards on page3

--- src/components/pages/page3.jsx
+++ src/components/pages/page3.jsx
@@ -22,27 +22,23 @@ function page3() {
             {post.map((p, index) => {
                 return (
                     <div key={index} className='col-12 m-2 ms-0 pb-lg-0'>
-                        <div className='row pt-0'>
+                        <div className='row pt-0 p-0 p-lg-1 ps-lg-0'>
                             <div className='col-lg-5 col-12 pe-1 pe-lg-0'>
                                 <img src={p.image.url} alt={p.image.alt} className='img-fluid w-100 h-100 object-fit-cover ' />
                             </div>
-                            <div className='bg-light p-3 mx-auto mx-lg-0 m-lg-3 ms-lg-0 col-lg-6 col-10 d-flex flex-column justify-content-between custom-card-border' >
-                                <div>
+                            <div className='bg-light p-3 mx-auto mx-lg-0 m-lg-3 ms-lg-0 col-lg-7 col-10 d-flex flex-column justify-content-between custom-card-border' >
+                                <div className='d-flex flex-column gap-2'>
                                     <h6 className='text-muted text-uppercase mt-lg-3  mb-0 '>{p.subtitle[language]}</h6>
                                     <h4 className='mb-1 fw-semibold'>{p.title[language]}</h4>
                                     <h6 className='overflow-hidden mb-3 m-lg-0' style={{ display: '-webkit-box', WebkitLineClamp: 3, WebkitBoxOrient: 'vertical', textOverflow: 'ellipsis' }}>
                                         {p.description[language]}
                                     </h6>
                                 </div>
-
-                                <div>
-                                    <div className='d-flex align-items-baseline gap-2'>
-                                        <p className='text-muted small mb-0  lh-1'>
-                                            {p.data[language]} - {p.local[language]}
-                                        </p>
-                                        <button className='icon-link btn btn-link ms-auto ms-lg-auto p-0 mb-0 mb-lg-3 pe-lg-3 text-nowrap flex-shrink-0 align-self-baseline' onClick={() => navigate('/inPost', { state: p })}>{labels.viewPost[language]}</button>
-                                    </div>
-
+                                <div className='d-flex align-items-baseline gap-2 mt-auto'>
+                                    <p className='text-muted small mb-0  lh-1'>
+                                        {p.data[language]} - {p.local[language]}
+                                    </p>
+                                    <button className='icon-link btn btn-link ms-auto ms-lg-auto p-0 mb-0 mb-lg-3  text-nowrap flex-shrink-0 align-self-baseline' onClick={() => navigate('/inPost', { state: p })}>{labels.viewPost[language]}</button>
                                 </div>
                             </div>
                         </div>
